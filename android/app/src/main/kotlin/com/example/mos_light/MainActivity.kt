4f34ac351d53bb44d95b8ff7ea871a7d04578061
Yun-sooyong <wood5186@naver.com>
2022-08-08 19:17:43 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/mos_light/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mos_light
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:torch_light/torch_light.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isTorchOn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: const Text('lumen'),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+        elevation: 0,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.blueGrey,
+        child: Center(
+          child: IconButton(
+            icon: isTorchOn
+                ? const Icon(
+                    Icons.flashlight_on_rounded,
+                    size: 40,
+                    color: Colors.amber,
+                  )
+                : const Icon(
+                    Icons.flashlight_off_rounded,
+                    size: 40,
+                  ),
+            onPressed: () async {
+              setState(() {
+                isTorchOn = !isTorchOn;
+              });
+              isTorchOn ? _turnOnTorch(context) : _turnOffTorch(context);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _turnOnTorch(BuildContext context) async {
+    try {
+      await TorchLight.enableTorch();
+    } on Exception catch (_) {
+      _showMessage('Could not turn on torch', context);
+    }
+  }
+
+  Future<void> _turnOffTorch(BuildContext context) async {
+    try {
+      await TorchLight.disableTorch();
+    } on Exception catch (_) {
+      _showMessage('Could not disable torch', context);
+    }
+  }
+
+  void _showMessage(String message, BuildContext context) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+}
